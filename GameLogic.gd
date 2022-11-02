1881b9aa4df2aea1739e18d56e9c886984f61162
Timothy Stiles <Patashu@hotmail.com>
2022-11-02 22:38:25 +1100

    undo buffer and multiplier graphics

--- GameLogic.gd
+++ GameLogic.gd
@@ -10,7 +10,9 @@ var hero_hp = 100;
 var hero_atk = 1;
 var hero_def = 0;
 var hero_turn = 0;
+var hero_keypresses = 0;
 var has_won = false;
+var undo_buffer = [];
 
 func _ready() -> void:
 	# setup hero info
@@ -23,7 +25,10 @@ func update_hero_info() -> void:
 	heroinfo.text = "HP: " + str(hero_hp) + "\r\n";
 	heroinfo.text += "ATK: " + str(hero_atk) + "\r\n";
 	heroinfo.text += "DEF: " + str(hero_def) + "\r\n";
-	heroinfo.text += "Turn: " + str(hero_turn) + "\r\n";
+	if (hero_turn != hero_keypresses):
+		heroinfo.text += "Turn: " + str(hero_turn) + " (" + str(hero_keypresses) + ")" + "\r\n";
+	else:
+		heroinfo.text += "Turn: " + str(hero_turn) + "\r\n";
 	if (has_won):
 		heroinfo.text += "You have won!";
 
@@ -39,18 +44,43 @@ func move_hero(dir: Vector2) -> void:
 	if (floor_dest == floormap.tile_set.find_tile_by_name("Win")):
 		can_move = true;
 		has_won = true;
+		add_undo_event(["win"]);
 	if (can_move):
 		move_hero_commit(dir);
 		
 func move_hero_commit(dir: Vector2) -> void:
+	add_undo_event(["move", hero_loc]);
+	move_hero_silently(dir);
+	hero_turn += 1;
+	hero_keypresses += 1;
+	update_hero_info();
+
+func move_hero_silently(dir: Vector2) -> void:
 	actormap.set_cellv(hero_loc, -1);
 	hero_loc += dir;
 	actormap.set_cellv(hero_loc, actormap.tile_set.find_tile_by_name("Player"));
-	hero_turn += 1;
+
+func add_undo_event(event: Array) -> void:
+	if (undo_buffer.size() <= hero_turn):
+		undo_buffer.append([]);
+	undo_buffer[hero_turn].append(event);
+
+func undo() -> void:
+	if (hero_turn <= 0):
+		return
+	var events = undo_buffer.pop_back();
+	for event in events:
+		if (event[0] == "move"):
+			move_hero_silently(event[1] - hero_loc);
+		elif (event[0] == "win"):
+			has_won = false;
+	hero_turn -= 1;
+	hero_keypresses += 1;
 	update_hero_info();
 
 func _process(delta: float) -> void:
-	# 2) check for input, move hero around, process interactions, create undo buffer
+	if (Input.is_action_just_pressed("undo")):
+		undo();
 	if (has_won):
 		return;
 	if (Input.is_action_just_pressed("ui_left")):
