b819a5397bf8aa31d63c27d78272e55fb289b099
Timothy Stiles <Patashu@hotmail.com>
2023-02-11 12:25:13 +1100

    improve bound of MAX ATK

--- GameLogic.gd
+++ GameLogic.gd
@@ -349,7 +349,7 @@ func check_secret_endings() -> void:
 		secret_endings["OUCH"] = true;
 	if (hero_hp >= 1796 and !green_hero):
 		secret_endings["MAX HP"] = true;
-	if (hero_atk >= 93 and !green_hero):
+	if (hero_atk >= 94 and !green_hero):
 		secret_endings["MAX ATK"] = true;
 	if (hero_def >= 73 and !green_hero):
 		secret_endings["MAX DEF"] = true;
